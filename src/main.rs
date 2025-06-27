0825fad509954db43734efb6ac1c79557b03b4f0
Ned Santiago <nedsantiago@proton.me>
2025-06-27 11:34:34 +0800

    Able to iterate through image pixels

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,17 @@ use std::error::Error;
 fn main() -> Result<(), Box<dyn Error>> {
     // Open an image file
     // Convert the image to RGBA
+    // Make an array of rgba filtering out half alphas
     let img = "/home/nix-admin/.config/bg-img/wallhaven-ox6d57_1920x1080.png";
     let img: image::RgbaImage = img_io::open_img_rgba(img)?;
     // Get Width image
     // Get Height of image
+    // Get number of pixels h x w
     let img_dimensions: data_models::Dimensions = img.dimensions();
-    println!("Dimensions: {:?}", img_dimensions);
+
+    for pixel in img.pixels() {
+        println!("{:?}", pixel);
+    }
 
     Ok(())
-    // Get number of pixels h x w
-    // Make an aray of rgba filtering out half alphas
 }
