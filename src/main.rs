2a49e7c02c5572e529edc834333c508973c8583e
Ned Santiago <nedsantiago@proton.me>
2025-06-30 17:50:38 +0800

    Small changes to comments

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,15 @@ use image::{ Pixel, Rgba };
 
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // Open an image file as RGBA
-    // Convert the image to RGBA
+    // NOTE FUTURE: Take file as cli argument
     let img_path = "/home/nix-admin/.config/bg-img/wallhaven-ox6d57_1920x1080.png";
+    // Open and Convert the image to RGBA
     let img: image::RgbaImage = img_io::open_img_rgba(img_path)?;
 
     // Get number of pixels h x w
     let total_pixel_count: u32 = img.width() * img.height();
 
-    // May Add Failure mode: Image too large
+    // NOTE FUTURE: Add Failure mode when Image too large
 
     // Make an array of rgba filtering out half alphas
     let mut valid_pixels = Vec::new();
@@ -29,7 +29,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     // Calculate colors
-
 
     Ok(())
 }
