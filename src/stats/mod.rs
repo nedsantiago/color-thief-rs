846cc4ef2011040ff2c36590adcfdf3b8b71dbcd
Ned Santiago <nedsantiago@proton.me>
2025-07-15 00:50:11 +0800

    Added a test case for calc_minmax_box

--- src/stats/mod.rs
+++ src/stats/mod.rs
@@ -1,9 +1,10 @@
 use std::collections::HashMap;
 use crate::data_models::Histogram;
 use crate::data_models::FrequencyMap;
+use crate::data_models::MinMaxBox;
 
 
-fn calc_histogram(pixels: Vec<Rgba<u8>>) -> Histogram {
+fn calc_histogram(data: Vec<Rgba<u8>>) -> Histogram {
     Histogram(
         vec![
             1, 1, 1, 1, 1,
@@ -12,7 +13,7 @@ fn calc_histogram(pixels: Vec<Rgba<u8>>) -> Histogram {
     )
 }
 
-fn calc_frequency_map(pixels: Vec<Rgba<u8>>) -> FrequencyMap {
+fn calc_frequency_map(data: Vec<Rgba<u8>>) -> FrequencyMap {
     FrequencyMap(
         HashMap::from([
             (32767, 1), (31710, 1),
@@ -22,6 +23,17 @@ fn calc_frequency_map(pixels: Vec<Rgba<u8>>) -> FrequencyMap {
             (23254, 1), (22197, 2),
         ])
     )
+}
+
+fn calc_minmax_box(data: Vec<Rgba<u8>>) -> MinMaxBox {
+    MinMaxBox {
+        rmin: 0,
+        rmax: 31,
+        gmin: 0,
+        gmax: 31,
+        bmin: 0,
+        bmax: 30,
+    }
 }
 
 use image::Rgba;
@@ -49,7 +61,7 @@ mod test_stats {
     }
 
     #[test]
-    fn test_get_frequency_map() {
+    fn test_calc_frequency_map() {
         let input = vec![
             Rgba::from([31 as u8; 4]), Rgba::from([30 as u8; 4]),
             Rgba::from([29 as u8; 4]), Rgba::from([28 as u8; 4]),
@@ -67,6 +79,33 @@ mod test_stats {
                 (23254, 1), (22197, 1),
             ])
         ).0;
-        assert_eq!(expected, found, "Logic Error");
+        assert_eq!(expected, found, "Logic Error:");
+    }
+
+    #[test]
+    fn test_calc_minmax_box() {
+        let input = vec![
+            Rgba::from([31 as u8; 4]), Rgba::from([30 as u8; 4]),
+            Rgba::from([29 as u8; 4]), Rgba::from([28 as u8; 4]),
+            Rgba::from([27 as u8; 4]), Rgba::from([26 as u8; 4]),
+            Rgba::from([25 as u8; 4]), Rgba::from([24 as u8; 4]),
+            Rgba::from([23 as u8; 4]), Rgba::from([22 as u8; 4]),
+        ];
+        let found = calc_minmax_box(input);
+        let expected = MinMaxBox {
+            rmin: 0,
+            rmax: 31,
+            gmin: 0,
+            gmax: 31,
+            bmin: 0,
+            bmax: 31,
+        };
+    
+        assert_eq!(expected.rmin, found.rmin, "Logic Error: rmin");
+        assert_eq!(expected.rmax, found.rmax, "Logic Error: rmax");
+        assert_eq!(expected.gmin, found.gmin, "Logic Error: gmin");
+        assert_eq!(expected.gmax, found.gmax, "Logic Error: gmax");
+        assert_eq!(expected.bmin, found.bmin, "Logic Error: bmin");
+        assert_eq!(expected.bmax, found.bmax, "Logic Error: bmax");
     }
 }
